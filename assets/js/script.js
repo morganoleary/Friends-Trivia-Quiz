e8262c589dade27ad4358b596eb88a6e918d57b6
morganoleary <137559549+morganoleary@users.noreply.github.com>
2023-09-24 14:40:46 +0000

    Add basic structure and styling of quiz

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let startButton = document.getElementsByClassName('start-button');
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+function incrementCorrectAnswer() {
+
+}
+
+function incrementIncorrectAnswer() {
+
+}
+
