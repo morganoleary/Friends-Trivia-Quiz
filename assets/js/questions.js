3999c5f688d2463ca4222375aa94635f360238a9
morganoleary <137559549+morganoleary@users.noreply.github.com>
2023-10-01 23:27:18 +0000

    Move questions array to new questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,94 @@
+// Quiz questions and answers
+export const questions = [
+    {
+        question: "What is the name of Phoebe's alter-ego?",
+        answers: [
+            { text: 'Phoebe Neeby', correct: false },
+            { text: 'Monica Bing', correct: false },
+            { text: 'Regina Phalange', correct: true },
+            { text: 'Elaine Benes', correct: false },
+        ]
+    },
+    {
+        question: "What is Janice most likely to say?",
+        answers: [
+            { text: 'Talk to the hand!', correct: false },
+            { text: 'Oh... my... God!', correct: true },
+            { text: 'Get me a coffee!', correct: false },
+            { text: 'No way!', correct: false },
+        ]
+    },
+    {
+        question: "What is Chandler's middle name?",
+        answers: [
+            { text: 'Muriel', correct: true },
+            { text: 'Jason', correct: false },
+            { text: 'Kim', correct: false },
+            { text: 'Zachary', correct: false },
+        ]
+    },
+    {
+        question: "What song is Phoebe best known for?",
+        answers: [
+            { text: 'Smelly Dog', correct: false },
+            { text: 'Smelly Cat', correct: true },
+            { text: 'Smelly Rabbit', correct: false },
+            { text: 'Smelly Worm', correct: false },
+        ]
+    },
+    {
+        question: "What is the name of Joey's penguin?",
+        answers: [
+            { text: 'Snowflake', correct: false },
+            { text: 'Waddle', correct: false },
+            { text: 'Huggsy', correct: true },
+            { text: 'Bobber', correct: false },
+        ]
+    },
+    {
+        question: "Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?",
+        answers: [
+            { text: 'Sally Roberts', correct: false },
+            { text: 'Valerie Thompson', correct: false },
+            { text: 'Emily Foster', correct: false },
+            { text: 'Amy Welsh', correct: true },
+        ]
+    },
+    {
+        question: "What pet did Ross own?",
+        answers: [
+            { text: 'A dog named Keith', correct: false },
+            { text: 'A rabbit called Lancelot', correct: false },
+            { text: 'A monkey named Marcel', correct: true },
+            { text: 'A lizard named Alistair', correct: false },
+        ]
+    },
+    {
+        question: "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?",
+        answers: [
+            { text: 'Italy', correct: true },
+            { text: 'France', correct: false },
+            { text: 'England', correct: false },
+            { text: 'Greece', correct: false },
+        ]
+    },
+    {
+        question: "Who was Chandler's TV magazine always addressed to?",
+        answers: [
+            { text: 'Chanandler Bang', correct: false },
+            { text: 'Chanandler Bong', correct: true },
+            { text: 'Chanandler Bing', correct: false },
+            { text: 'Chanandler Beng', correct: false },
+        ]
+    },
+    {
+        question: "What is the name of Rachel's Sphynx cat?",
+        answers: [
+            { text: 'Baldy', correct: false },
+            { text: 'Sid', correct: false },
+            { text: 'Mrs. Whiskerson', correct: true },
+            { text: 'Felix', correct: false },
+        ]
+    },
+    // Adding more questions and answers
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
+import { questions } from '../assets/js/questions.js';
+
 // Get DOM elements
 const buttons = document.getElementsByClassName('btn')[0];
 const startButton = document.querySelector('.start-button');
 const questionElement = document.querySelector('.question-line');
 const answerButtons = document.querySelectorAll('.answer-option');
 const nextQuestionButton = document.querySelector('.next-question');
-const correctScoreElement = document.getElementsByClassName('score-correct');
-const incorrectScoreElement = document.getElementsByClassName('score-incorrect');
+const correctScoreElement = document.getElementById('score-correct');
+const incorrectScoreElement = document.getElementById('score-incorrect');
 let currentQuestionIndex = 0;
 let correctScore = 0;
 let incorrectScore = 0;
@@ -22,100 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Quiz questions and answers
-const questions = [
-    {
-        question: "What is the name of Phoebe's alter-ego?",
-        answers: [
-            { text: 'Phoebe Neeby', correct: false },
-            { text: 'Monica Bing', correct: false },
-            { text: 'Regina Phalange', correct: true },
-            { text: 'Elaine Benes', correct: false },
-        ]
-    },
-    {
-        question: "What is Janice most likely to say?",
-        answers: [
-            { text: 'Talk to the hand!', correct: false },
-            { text: 'Oh... my... God!', correct: true },
-            { text: 'Get me a coffee!', correct: false },
-            { text: 'No way!', correct: false },
-        ]
-    },
-    {
-        question: "What is Chandler's middle name?",
-        answers: [
-            { text: 'Muriel', correct: true },
-            { text: 'Jason', correct: false },
-            { text: 'Kim', correct: false },
-            { text: 'Zachary', correct: false },
-        ]
-    },
-    {
-        question: "What song is Phoebe best known for?",
-        answers: [
-            { text: 'Smelly Dog', correct: false },
-            { text: 'Smelly Cat', correct: true },
-            { text: 'Smelly Rabbit', correct: false },
-            { text: 'Smelly Worm', correct: false },
-        ]
-    },
-    {
-        question: "What is the name of Joey's penguin?",
-        answers: [
-            { text: 'Snowflake', correct: false },
-            { text: 'Waddle', correct: false },
-            { text: 'Huggsy', correct: true },
-            { text: 'Bobber', correct: false },
-        ]
-    },
-    {
-        question: "Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?",
-        answers: [
-            { text: 'Sally Roberts', correct: false },
-            { text: 'Valerie Thompson', correct: false },
-            { text: 'Emily Foster', correct: false },
-            { text: 'Amy Welsh', correct: true },
-        ]
-    },
-    {
-        question: "What pet did Ross own?",
-        answers: [
-            { text: 'A dog named Keith', correct: false },
-            { text: 'A rabbit called Lancelot', correct: false },
-            { text: 'A monkey named Marcel', correct: true },
-            { text: 'A lizard named Alistair', correct: false },
-        ]
-    },
-    {
-        question: "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?",
-        answers: [
-            { text: 'Italy', correct: true },
-            { text: 'France', correct: false },
-            { text: 'England', correct: false },
-            { text: 'Greece', correct: false },
-        ]
-    },
-    {
-        question: "Who was Chandler's TV magazine always addressed to?",
-        answers: [
-            { text: 'Chanandler Bang', correct: false },
-            { text: 'Chanandler Bong', correct: true },
-            { text: 'Chanandler Bing', correct: false },
-            { text: 'Chanandler Beng', correct: false },
-        ]
-    },
-    {
-        question: "What is the name of Rachel's Sphynx cat?",
-        answers: [
-            { text: 'Baldy', correct: false },
-            { text: 'Sid', correct: false },
-            { text: 'Mrs. Whiskerson', correct: true },
-            { text: 'Felix', correct: false },
-        ]
-    },
-    // Adding more questions and answers
-];
+
 
 // Quiz begins with click of "Begin Quiz" button
 function startQuiz() {
@@ -211,7 +120,7 @@ function selectAnswer() {
 
 // Function to update score tally
 function updateScoreDisplay() {
-    // Update corre score display
+    // Update correct score display
     correctScoreElement.textContent = correctScore;
     // Update incorrect score display
     incorrectScoreElement.textContent = incorrectScore;
